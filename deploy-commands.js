884bffdfbc714b3b113e4b78fdcfc032b9d922a7
NotGood-star <reetujoshi588@gmail.com>
2026-05-25 09:33:54 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,40 +4,78 @@ const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("help")
-    .setDescription("Help menu"),
 
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Ping command"),
+new SlashCommandBuilder()
+.setName("help")
+.setDescription("Help menu"),
 
-  new SlashCommandBuilder()
-    .setName("joke")
-    .setDescription("Funny joke")
+new SlashCommandBuilder()
+.setName("ping")
+.setDescription("Check bot ping"),
+
+new SlashCommandBuilder()
+.setName("joke")
+.setDescription("Get a funny joke"),
+
+new SlashCommandBuilder()
+.setName("dice")
+.setDescription("Roll a dice"),
+
+new SlashCommandBuilder()
+.setName("coinflip")
+.setDescription("Flip a coin"),
+
+new SlashCommandBuilder()
+.setName("quote")
+.setDescription("Get a random quote"),
+
+new SlashCommandBuilder()
+.setName("rps")
+.setDescription("Rock Paper Scissors")
+.addStringOption(option =>
+option.setName("choice")
+.setDescription("rock / paper / scissors")
+.setRequired(true)
+.addChoices(
+{ name: "rock", value: "rock" },
+{ name: "paper", value: "paper" },
+{ name: "scissors", value: "scissors" }
+)
+),
+
+new SlashCommandBuilder()
+.setName("guess")
+.setDescription("Guess number 1-10")
+.addIntegerOption(option =>
+option.setName("number")
+.setDescription("Choose a number")
+.setRequired(true)
+)
+
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 (async () => {
-  try {
 
-    console.log("TOKEN:", TOKEN ? "Loaded" : "Missing");
-    console.log("CLIENT_ID:", CLIENT_ID ? "Loaded" : "Missing");
+try {
 
-    console.log("Registering slash commands...");
+console.log("Registering slash commands...");
 
-    await rest.put(
-      Routes.applicationCommands(CLIENT_ID),
-      { body: commands }
-    );
+await rest.put(
+Routes.applicationCommands(CLIENT_ID),
+{ body: commands }
+);
 
-    console.log("Slash commands registered!");
+console.log("Slash commands registered!");
 
-    process.exit(0);
+process.exit(0);
 
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
+} catch (error) {
+
+console.error(error);
+process.exit(1);
+
+}
+
 })();
