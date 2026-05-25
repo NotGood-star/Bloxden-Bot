23889eba856f8b5a4e89a996fcac3c44ebb5208a
NotGood-star <reetujoshi588@gmail.com>
2026-05-25 08:59:34 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,32 +3,38 @@ require("dotenv").config();
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 
 const commands = [
-  new SlashCommandBuilder()
-    .setName("help")
-    .setDescription("Help command"),
 
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Ping command"),
+new SlashCommandBuilder()
+.setName("help")
+.setDescription("Help menu"),
 
-  new SlashCommandBuilder()
-    .setName("joke")
-    .setDescription("Funny joke")
-].map(cmd => cmd.toJSON());
+new SlashCommandBuilder()
+.setName("ping")
+.setDescription("Ping command"),
+
+new SlashCommandBuilder()
+.setName("joke")
+.setDescription("Funny joke")
+
+].map(command => command.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-  try {
-    console.log("Registering slash commands...");
 
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands }
-    );
+try {
 
-    console.log("Slash commands registered!");
-  } catch (error) {
-    console.error(error);
-  }
+console.log("Registering slash commands...");
+
+await rest.put(
+Routes.applicationCommands(process.env.CLIENT_ID),
+{ body: commands }
+);
+
+console.log("Slash commands registered!");
+
+} catch (error) {
+console.error(error);
+}
+
 })();
