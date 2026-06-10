d8b6b53b33ea6d6479488ae12c3981be36136e4b
Not_Good <reetujoshi588@gmail.com>
2026-06-10 07:50:38 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,32 +1,37 @@
-require("dotenv").config();
-const fs = require("fs");
-const { REST, Routes } = require("discord.js");
+const { REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const commands = [];
-const folders = fs.readdirSync("./commands");
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of folders) {
-    const files = fs.readdirSync(`./commands/${folder}`);
-
-    for (const file of files) {
-        const command = require(`./commands/${folder}/${file}`);
-        commands.push(command.data.toJSON());
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            commands.push(command.data.toJSON());
+        }
     }
 }
 
-const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+const rest = new REST().setToken(process.env.TOKEN);
 
 (async () => {
     try {
-        console.log("🚀 Deploying slash commands...");
-
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID),
-            { body: commands }
+            { body: commands },
         );
-
-        console.log("✅ Commands deployed!");
-    } catch (err) {
-        console.log(err);
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
     }
 })();
