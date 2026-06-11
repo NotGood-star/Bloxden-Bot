b9f45a4220109e299b2f3a458a17059f3ece57d6
Not_Good <reetujoshi588@gmail.com>
2026-06-11 08:16:54 +0530

    Update weekly.js

--- commands/levels/weekly.js
+++ commands/levels/weekly.js
@@ -1,22 +1,33 @@
-const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
 
 const dbPath = path.join(__dirname, '../../database.json');
-function readDB() { return JSON.parse(fs.readFileSync(dbPath, 'utf8') || '{}'); }
-function writeDB(data) { fs.writeFileSync(dbPath, JSON.stringify(data, null, 2)); }
+function readDB() { 
+    try {
+        return JSON.parse(fs.readFileSync(dbPath, 'utf8') || '{}'); 
+    } catch (e) {
+        return {};
+    }
+}
+function writeDB(data) { 
+    try {
+        fs.writeFileSync(dbPath, JSON.stringify(data, null, 2)); 
+    } catch (e) {}
+}
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('weekly')
         .setDescription('Weekly operations panel')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addSubcommand(sub =>
-            sub.setName('leaderboard-set')
-                .setDescription('Manually seed or override structural weekly score metrics')
-                .addUserOption(opt => opt.setName('user').setDescription('Target user').setRequired(true))
-                .setIntegerOption(opt => opt.setName('score').setDescription('The weekly score calculation').setRequired(true))
-        ),
+        .addSubcommand(subcommand => {
+            subcommand.setName('leaderboard-set');
+            subcommand.setDescription('Manually seed or override structural weekly score metrics');
+            subcommand.addUserOption(option => option.setName('user').setDescription('Target user').setRequired(true));
+            subcommand.setIntegerOption(option => option.setName('score').setDescription('The weekly score calculation').setRequired(true));
+            return subcommand;
+        }),
     async execute(interaction) {
         const target = interaction.options.getUser('user');
         const score = interaction.options.getInteger('score');
