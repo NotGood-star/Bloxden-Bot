c715b4a4e8a7d9154dfc81c7cf2b6969c567f7a5
Not_Good <reetujoshi588@gmail.com>
2026-06-14 21:39:03 +0530

    Update level-set.js

--- commands/levels/level-set.js
+++ commands/levels/level-set.js
@@ -1,10 +1,6 @@
 const { SlashCommandBuilder, PermissionFlagsBits, ChannelType } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-
-const dbPath = path.join(__dirname, '../../database.json');
-function readDB() { return JSON.parse(fs.readFileSync(dbPath, 'utf8') || '{}'); }
-function writeDB(data) { fs.writeFileSync(dbPath, JSON.stringify(data, null, 2)); }
+// Import the central database system
+const db = require('../../database.js'); 
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -14,16 +10,27 @@ module.exports = {
         .addSubcommand(sub =>
             sub.setName('set-channel')
                 .setDescription('Set the channel where level-up alerts are posted')
-                .addChannelOption(opt => opt.setName('channel').setDescription('The chat feed channel').addChannelTypes(ChannelType.GuildText).setRequired(true))
+                .addChannelOption(opt => 
+                    opt.setName('channel')
+                        .setDescription('The chat feed channel')
+                        .addChannelTypes(ChannelType.GuildText)
+                        .setRequired(true)
+                )
         ),
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
-        const db = readDB();
+        const guildId = interaction.guild.id;
+
+        // Use the centralized systemChannels map
+        // We store it using the guild ID to support multiple servers
+        db.systemChannels.set(`${guildId}-level`, channel.id);
         
-        if (!db.settings) db.settings = {};
-        db.settings.levelChannel = channel.id;
+        // Persist the changes to the JSON file immediately
+        db.saveDatabase();
         
-        writeDB(db);
-        return interaction.reply({ content: `🎯 Level up announcements will now be directed to ${channel}!` });
+        return interaction.reply({ 
+            content: `🎯 Level up announcements will now be directed to ${channel}!`,
+            ephemeral: true 
+        });
     }
 };
